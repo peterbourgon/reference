3a27beaba18f49c55280286074b0b9efda6836ac
Peter Bourgon <peter@bourgon.org>
2015-12-26 19:55:40 -0500

    bcrypt.GenerateFromPassword(..., 16) is slow!

--- crypto.go
+++ crypto.go
@@ -19,7 +19,7 @@ func salt(plaintextPassword string) (salt, digest string) {
 	}
 	salt = fmt.Sprintf("%X-%X-%X-%X-%X", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
 
-	hash, err := bcrypt.GenerateFromPassword([]byte(fixed+salt+plaintextPassword), 16)
+	hash, err := bcrypt.GenerateFromPassword([]byte(fixed+salt+plaintextPassword), 8)
 	if err != nil {
 		panic(err)
 	}
